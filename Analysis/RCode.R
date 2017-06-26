0b3b3b4fd4ef140a4646a0a7747fa57f08bff238
Mohan Nepal <mohan.nepal@hotmail.com>
2017-06-26 18:57:24 -0500

    added new code

--- Analysis/RCode.R
+++ Analysis/RCode.R
@@ -9,15 +9,25 @@ setwd("C:/Users/mohan/Desktop/MSDS6306_CaseStudy01/Data")
 # make sure you have your two files 
 list.files()
 # read and assign the bv variable for beers data set
-bv<- read.csv("Beers.csv",header= TRUE)
-brv <- read.csv("Breweries.csv",header = TRUE)
+bv<- read.csv("Beers.csv",header= TRUE,na.strings = "NA")
+brwv <- read.csv("Breweries.csv",header = TRUE, na.strings = "NA")
 # check the two data set that you just read from .csv files
 head(bv)
-head(brv)
-# calulate the numbers of breweries in each state, used to method to verify 
-brv.per.state1 <- data.frame(table(brv$State))
-brv.per.state2  <- aggregate(rep(1, length(brv$State)),by=list(brv$State), sum)
+head(brwv)
+# calulate the numbers of breweries in each state, used two method to verify 
+brwv.per.state1 <- data.frame(table(brwv$State))
+brwv.per.state2  <- aggregate(rep(1, length(brwv$State)),by=list(brwv$State), sum)
+
+# numbers of NA in each column
+missing.bv <- apply(bv,2, function(x){sum(is.na(x))})
+missing.brwv <- apply(brwv,2, function(x){sum(is.na(x))})
+
+# merge beers and breweries by Brewery_id
+combined.data <- merge(bv, brwv, by.bv= bv$Brewery_id, by.brwv=brwv$Brew_ID, all.x = TRUE)
 
 
+missing.combined <- apply(combined.data, 2, function(x){ sum(is.na(x))})
+
+#
 
 
